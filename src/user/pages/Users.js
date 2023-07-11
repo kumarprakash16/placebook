2588aed6df9823776f9a51c6c53e59d6bd844290
kumarprakash16 <kumarprakash1636@gmail.com>
2023-07-11 11:46:59 +0530

    video 41

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import {UsersList} from '../components/UsersList' 
 
 export const Users = () =>{
+    const USERS = [
+        {
+            id: 'u',
+            name: 'Prakash Kumar',
+            image: 'https://ylpapp.isb.edu/user/sample_photo.html',
+            places: 3
+        }
+    ]
     return(
-        <h2>Hey</h2>
+        <UsersList items={USERS}/>
+
     )
 } 
